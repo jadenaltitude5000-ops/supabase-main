ef68385a6e8f0bcfcb7ef9e9e8f15e10f3d9c346
Jaden Altitude <jadenaltitude5000@gmail.com>
2025-12-04 23:06:45 +0000

    Google
    Enable Sign in with Google
    Enables Sign in with Google on the we

--- next.config.js
+++ next.config.js
@@ -28,6 +28,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
   // This will ignore ESLint errors during the build
